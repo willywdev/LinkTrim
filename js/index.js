a4527c6a10d81ea4a7a8afd1d64023b939a93405
Wilhelm Werwein <werwein.dev@gmail.com>
2023-07-14 20:21:15 +0200

    call api + change dom elements to display api results

--- js/index.js
+++ js/index.js
@@ -1,16 +1,34 @@
+/* Code from API Documentation */
 var myHeaders = new Headers();
 myHeaders.append("apikey", config.MY_KEY);
-
-var raw = "{body}";
+let urlResult = undefined;
+/* * Getting DOM Elements */
+const inputField = document.querySelector("[data-js='inputField']");
+const outputField = document.querySelector("[data-js='outputField']");
+const formElement = document.querySelector("[data-js='formElement']");
+window.addEventListener("load", () => formElement.reset());
+/* Starting DOM Manipulation */
+formElement.addEventListener("submit", () => {
+  requestOptions.body = inputField.value;
+  console.log(requestOptions.body);
+  getShortURL();
+});
+/* Code from API Documentation */
 
 var requestOptions = {
   method: "POST",
   redirect: "follow",
   headers: myHeaders,
-  body: "willydev.de",
+  body: "",
 };
-
-fetch("https://api.apilayer.com/short_url/hash", requestOptions)
-  .then((response) => response.text())
-  .then((result) => console.log(result))
-  .catch((error) => console.log("error", error));
+const getShortURL = () => {
+  fetch("https://api.apilayer.com/short_url/hash", requestOptions)
+    .then((response) => response.text())
+    .then((outputField.value = "Waiting"))
+    .then((result) => {
+      const jsonResult = JSON.parse(result);
+      console.log(jsonResult);
+      outputField.value = jsonResult.short_url;
+    })
+    .catch((error) => console.log("error", error));
+};
