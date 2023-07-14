2998b06528e87cc88e3ae5ba3b08496f96f30965
Wilhelm Werwein <werwein.dev@gmail.com>
2023-07-14 21:45:52 +0200

    change style, add copy button

--- js/index.js
+++ js/index.js
@@ -1,21 +1,44 @@
-/* Code from API Documentation */
+// * Code from API Documentation
 var myHeaders = new Headers();
 myHeaders.append("apikey", config.MY_KEY);
 let urlResult = undefined;
-/* * Getting DOM Elements */
+
+// * Getting DOM Elements
 const inputField = document.querySelector("[data-js='inputField']");
 const outputField = document.querySelector("[data-js='outputField']");
 const formElement = document.querySelector("[data-js='formElement']");
 const loadingIcon = document.querySelector("[data-js='loadingIcon']");
+const userMessage = document.querySelector("[data-js='usermsg']");
+const copyButton = document.querySelector("[data-js='copyBtn']");
+let p = document.createElement("p");
 
+// * Starting DOM Manipulation
 window.addEventListener("load", () => formElement.reset());
-/* Starting DOM Manipulation */
+
 formElement.addEventListener("submit", () => {
   loadingIconInsert();
   requestOptions.body = inputField.value;
   getShortURL();
+  p.remove();
 });
 
+copyButton.addEventListener("click", () => {
+  if (outputField.value.length === 0) {
+    userMessage.appendChild(p);
+    p.className = "errorMessage";
+    console.log(p);
+    p.textContent = "There is nothing to copy 😥";
+  } else {
+    outputField.select();
+    outputField.setSelectionRange(0, 99999);
+    navigator.clipboard.writeText(outputField.value);
+    userMessage.appendChild(p);
+    p.textContent = "URL copied! 📋";
+    p.className = "copyMessage";
+  }
+});
+
+// * FUNCTIONS
 function loadingIconInsert() {
   let img = document.createElement("img");
   loadingIcon.appendChild(img);
@@ -27,8 +50,8 @@ function deletingIcon() {
   let img = document.querySelector(".loading-icon");
   img.remove();
 }
-/* Code from API Documentation */
 
+//* Code from API Documentation
 var requestOptions = {
   method: "POST",
   redirect: "follow",
